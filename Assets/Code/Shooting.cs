0eeff2f9a323cd4348a37891d08ed782bd5eef09
IndiSnacks <65885329+IndiSnacks@users.noreply.github.com>
2021-02-28 23:55:52 -0500

    Fire Action works
    Void will shoot once per botton press with this code

--- Assets/Code/Shooting.cs
+++ Assets/Code/Shooting.cs
@@ -6,29 +6,28 @@ using UnityEngine.InputSystem;
 public class Shooting : MonoBehaviour
 {
     private bool shootbool;
+    private float time = 0f;
     public Transform firepoint;
     public GameObject bulletPrefab;
     public float bulletForce = 200f;
-    
 
-    private void shoot(InputAction.CallbackContext shootValue)
+    public void shoot(InputAction.CallbackContext shootValue)
     {
-        shootbool = shootValue.ReadValueAsButton();
+        switch(shootValue.phase)
+        {
+            case InputActionPhase.Started:
+                {
+                    shooted();
+                }
+                break;
+        }
     }
 
     private void shooted()
     {
-      GameObject Projectile = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
-      Rigidbody2D rbp = Projectile.GetComponent<Rigidbody2D>();
-      rbp.AddForce(firepoint.right * bulletForce, ForceMode2D.Impulse);
-      Debug.Log("Shoot");
+        GameObject Projectile = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+        Rigidbody2D rbp = Projectile.GetComponent<Rigidbody2D>();
+        rbp.AddForce(firepoint.right * bulletForce, ForceMode2D.Impulse);
     }
 
-    private void FixedUpdate() 
-    {
-        if(shootbool == true)
-        {
-            shooted();
-        }
-    }
 }
